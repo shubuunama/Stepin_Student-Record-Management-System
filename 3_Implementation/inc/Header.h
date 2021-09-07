97f913de3848b2719f4ec70c7192cb8453432b3c
shubuunama <89735311+shubuunama@users.noreply.github.com>
2021-09-08 01:46:48 +0530

    Create Header.h

---
+++ 3_Implementation/inc/Header.h
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<string.h>
+
+void gotoxy(int ,int );
+void menu();
+void add();
+void view();
+void search();
+void modify();
+void deleterec();
+struct student
+{
+    char name[20];
+    char mobile[10];
+    int rollno;
+    char course[20];
+    char branch[20];
+};
+
+int main()
+{
+  /* requried details */
+}
+
+void menu()
+{
+  // all cases
+}
+
+void add()
+{
+  
+  //enter details
+  
+}
+
+void view()
+{
+//display record   
+}
+
+void Search()
+{
+  //searching data
+}
+
+void modify()
+{
+  //modify details
+  
+}
+
+void deleterec()
+{
+  //delete record
+}
+
+void gotoxy(int x,int y)
+{
+  //getting output
+}
+
